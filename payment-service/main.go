4d58cc0972e265fb4bfd49cd0e3cd49758db9bfa
denny <denny.sinulingga@ottodigital.id>
2023-03-18 15:01:10 +0700

    update

--- payment-service/main.go
+++ payment-service/main.go
@@ -17,18 +17,18 @@ func main() {
 		},
 	}
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 
-			for j := 0; j < 1000; j++ {
+			for j := 0; j < 100; j++ {
 				body := []byte(`{
 					"name": "Macbook Pro 13 M1 32 GB 1 TB",
 					"qtty": 10,
 					"price": 73000000
 				}`)
-				request, errNewRequest := http.NewRequest(http.MethodPost, "http://127.0.0.1:8081/product", bytes.NewBuffer(body))
+				request, errNewRequest := http.NewRequest(http.MethodPost, "http://localhost/api/v1/products", bytes.NewBuffer(body))
 				if errNewRequest != nil {
 					log.Printf("Error new request: %v", errNewRequest)
 				}
